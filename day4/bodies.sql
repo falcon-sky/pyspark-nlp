161f2c0f01bd6e1ddbc609ef10c458926b869c33
Rama Shanker <rama.shanker@wirelesscar.com>
2022-02-17 09:51:26 +0100

    added mysql configuration

---
+++ day4/bodies.sql
@@ -0,0 +1,33 @@
+CREATE TABLE bodies (id INT NOT NULL AUTO_INCREMENT,
+			height INT, 
+			weight INT,
+			gender VARCHAR(20),
+			PRIMARY KEY ( id ));
+			
+INSERT INTO bodies 
+    (height, weight, gender) 
+VALUES 
+    (182, 86, "M"),
+	(193,112, "M"),
+	(172, 72, "M"),
+	(170, 61, "F"),
+	(167, 77, "F"),
+	(165, 59, "F"),
+	(177, 88, "M"),
+	(162, 58, "F"),
+	(165, 54, "F"),
+	(176, 88, "M"),
+	(167, 50, "F"),
+	(167, 58, "F"),
+	(179, 56, "M"),
+	(167, 55, "F"),
+	(177, 78, "M"),
+	(182, 74, "M"),
+	(186, 75, "M"),
+	(167, 61, "F"),
+	(167, 60, "F"),
+	(167 ,58, "F"),
+	(188, 88, "M"),
+	(167, 52, "F"),
+	(167, 53 ,"F");
+	
